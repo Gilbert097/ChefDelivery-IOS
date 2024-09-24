73bb2b0d2a877b5247a8bd8e11b05dba30516bec
Gilberto Silva <betosilva.luz@gmail.com>
2024-09-24 16:48:28 -0300

    Implementando layout da tela de detalhe dos restaurantes,

--- ChefDelivery/ChefDelivery/StoresView/StoreDetailView.swift
+++ ChefDelivery/ChefDelivery/StoresView/StoreDetailView.swift
@@ -11,7 +11,44 @@ struct StoreDetailView: View {
     let store:  StoreType
     
     var body: some View {
-        Text(store.name)
+        VStack(alignment: .leading) {
+            Image(store.headerImage)
+                .resizable()
+                .scaledToFit()
+            
+            HStack {
+                Text(store.name)
+                    .bold()
+                    .font(.title)
+                
+                Spacer()
+                
+                Image(store.logoImage)
+            }
+            .padding(.vertical, 8)
+            .padding(.horizontal)
+            
+            HStack {
+                Text(store.location)
+                
+                Spacer()
+                
+                ForEach(1...store.stars, id: \.self) { _ in
+                    
+                    Image(systemName: "star.fill")
+                        .foregroundStyle(.yellow)
+                        .font(.caption2)
+                }
+            }
+            .padding(.vertical, 8)
+            .padding(.horizontal)
+            
+            Text("Produtos")
+                .font(.title2)
+                .bold()
+                .padding()
+        }
+        .navigationTitle(store.name)
     }
 }
 
