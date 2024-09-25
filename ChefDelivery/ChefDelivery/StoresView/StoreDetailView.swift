9e354083c209bc06532e91757d08e4c06f22427f
Gilberto Silva <betosilva.luz@gmail.com>
2024-09-25 16:38:41 -0300

    Exibindo lista de produtos.

--- ChefDelivery/ChefDelivery/StoresView/StoreDetailView.swift
+++ ChefDelivery/ChefDelivery/StoresView/StoreDetailView.swift
@@ -11,44 +11,75 @@ struct StoreDetailView: View {
     let store:  StoreType
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Image(store.headerImage)
-                .resizable()
-                .scaledToFit()
-            
-            HStack {
-                Text(store.name)
-                    .bold()
-                    .font(.title)
+        ScrollView {
+            VStack(alignment: .leading) {
+                Image(store.headerImage)
+                    .resizable()
+                    .scaledToFit()
                 
-                Spacer()
-                
-                Image(store.logoImage)
-            }
-            .padding(.vertical, 8)
-            .padding(.horizontal)
-            
-            HStack {
-                Text(store.location)
-                
-                Spacer()
-                
-                ForEach(1...store.stars, id: \.self) { _ in
+                HStack {
+                    Text(store.name)
+                        .bold()
+                        .font(.title)
                     
-                    Image(systemName: "star.fill")
-                        .foregroundStyle(.yellow)
-                        .font(.caption2)
+                    Spacer()
+                    
+                    Image(store.logoImage)
+                }
+                .padding(.vertical, 8)
+                .padding(.horizontal)
+                
+                HStack {
+                    Text(store.location)
+                    
+                    Spacer()
+                    
+                    ForEach(1...store.stars, id: \.self) { _ in
+                        
+                        Image(systemName: "star.fill")
+                            .foregroundStyle(.yellow)
+                            .font(.caption2)
+                    }
+                }
+                .padding(.vertical, 8)
+                .padding(.horizontal)
+                
+                Text("Produtos")
+                    .font(.title2)
+                    .bold()
+                    .padding()
+                
+                ForEach(store.products) { product in
+                    
+                    HStack(spacing: 8) {
+                        
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text(product.name)
+                                .bold()
+                            
+                            Text(product.description)
+                                .foregroundStyle(.black.opacity(0.5))
+                            
+                            Text(String(product.price))
+                        }
+                        
+                        Spacer()
+                        
+                        Image(product.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 120, height: 90)
+                            .clipShape(RoundedRectangle(cornerRadius: 12))
+                            .overlay(RoundedRectangle(cornerRadius: 12)
+                                      .stroke(Color.black, lineWidth: 0)
+                                )
+                            .shadow(color: .black.opacity(0.3), radius: 20, x: 6, y: 8)
+                    }
+                    .padding()
                 }
             }
-            .padding(.vertical, 8)
-            .padding(.horizontal)
-            
-            Text("Produtos")
-                .font(.title2)
-                .bold()
-                .padding()
-        }
-        .navigationTitle(store.name)
+            .navigationTitle(store.name)
+        }.scrollIndicators(.hidden)
     }
 }
 
