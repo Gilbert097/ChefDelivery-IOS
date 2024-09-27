b403519d1ad31727f96baadfa475c3ecb9df54bd
Gilberto Silva <betosilva.luz@gmail.com>
2024-09-27 14:54:44 -0300

    Criando um menu para filtrar restaurantes.

--- ChefDelivery/ChefDelivery/StoresView/StoresContainerView.swift
+++ ChefDelivery/ChefDelivery/StoresView/StoresContainerView.swift
@@ -8,10 +8,34 @@
 import SwiftUI
 
 struct StoresContainerView: View {
+    
+    @State private var indexFilterSelected: Int = 0
+    
     var body: some View {
         VStack(alignment: .leading) {
-            Text("Lojas")
-                .font(.headline)
+            
+            HStack {
+                Text("Lojas")
+                    .font(.headline)
+                
+                Spacer()
+                
+                Menu("Filtrar") {
+                    
+                    ForEach(1...5, id: \.self) { index in
+                        Button {
+                            self.indexFilterSelected = index
+                        } label: {
+                            if index > 1 {
+                                Text("\(index) estrelas ou mais")
+                            } else {
+                                Text("\(index) estrela ou mais")
+                            }
+                        }
+
+                    }
+                }
+            }
             
             VStack(alignment: .leading, spacing: 30) {
                 ForEach(storesMock) { storeItem in
