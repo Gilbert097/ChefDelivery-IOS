365d60ee1e35632b2242d323d9bf88ec93313297
Gilberto Silva <betosilva.luz@gmail.com>
2024-09-30 16:59:23 -0300

    Utilizando timer no carrossel.

--- ChefDelivery/ChefDelivery/CarouselView/CarouselTabView.swift
+++ ChefDelivery/ChefDelivery/CarouselView/CarouselTabView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct CarouselTabView: View {
+    
+    @State private var selectionIndex = 1
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selectionIndex) {
             ForEach(carouselItens) {
                 CarouselItemView(orderType: $0)
+                    .tag($0.id)
             }
         }
         .frame(height: 180)
         .tabViewStyle(.page(indexDisplayMode: .always))
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+                if selectionIndex > carouselItens.count {
+                    selectionIndex = 1
+                }
+                selectionIndex += 1
+            }
+        }
     }
 }
 
